db2c6119ef29008dc603d64c988e8fcc6ce43e27
JhonnyDeveloper <jhonny.developer.256@gmail.com>
2024-11-14 21:47:00 -0500

    #main: ajuste de pruebas unitarias

--- back/tests/api/email_route_test.py
+++ back/tests/api/email_route_test.py
@@ -1,20 +1,37 @@
+from unittest.mock import MagicMock
 from api.main import app
 from fastapi.testclient import TestClient
-from unittest.mock import Mock, patch
+
+from core.service.email_service import EmailService
 
 client = TestClient(app)
 
 
 def test_send_email_success(email_model):
-    with patch("api.routes.email.EmailService.send", return_value=True) as mock_service:
+    mock_email_service = MagicMock(EmailService)
 
-        response = client.post(
-            "/email",
-            json=email_model.model_dump()
-        )
+    mock_email_service.send.return_value = True
 
-        assert response.status_code == 200
-        assert response.json() == {}
+    response = client.post(
+        "/email",
+        json=email_model.model_dump()
+    )
+
+    mock_email_service.reset_mock()
+
+    assert response.status_code == 200
+    assert response.json() == {}
+
+# def test_send_email_success(email_model):
+#     with patch("api.routes.email.EmailService.send", return_value=True) as mock_service:
+
+#         response = client.post(
+#             "/email",
+#             json=email_model.model_dump()
+#         )
+
+#         assert response.status_code == 200
+#         assert response.json() == {}
 
 
 # def test_send_email_failure(email_model):
